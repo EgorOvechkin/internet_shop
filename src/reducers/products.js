590db33cbf7f8535ce6eda5f42440bbcb2b79cf1
Egor Ovechkin <egorka1391@gmail.com>
2017-04-16 21:39:40 +0500

    used ramda in reducer

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,3 +1,5 @@
+import Ramda from 'ramda'
+
 export const RECIEVE_PRODUCTS = 'internet_shop/products/recieve_products'
 export const RECIEVE_PRODUCT = 'internet_shop/products/recieve_product'
 
@@ -12,19 +14,11 @@ const Product = {
 export default function reducer(state = {}, action = {}) {
   switch (action.type) {
     case RECIEVE_PRODUCT:
-      // const id = action.payload.id
-      //TODO
-      const newPro = {}
-      const product = Object.assign({}, Product, action.payload)
-      newPro[action.payload.id] = product
-      return Object.assign({}, state, newPro)
+      const product = Ramda.merge(Product, action.payload)
+      return Ramda.assoc(action.payload.id, product, state)
     case RECIEVE_PRODUCTS:
-      const actions = action.payload.map(product => recieveProduct(product))
-      return actions.reduce(
-        (currentState, currentAction) => reducer(currentState, currentAction),
-        state
-      )
-      // return Object.assign({}, state, { id: recieveProduct() })
+      const actions = Ramda.map(recieveProduct, action.payload)
+      return Ramda.reduce(reducer, state, actions)
     default:
       return state
   }
