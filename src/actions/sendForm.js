f4c479afc9ce082e1c17e165fc3704478b854774
EgorOvechkin <egorka1391@gmail.com>
2017-07-13 10:39:18 +0500

    Update sendForm.js

--- src/actions/sendForm.js
+++ src/actions/sendForm.js
@@ -2,7 +2,6 @@ import { setOrderFormStatus, resetState } from '../actions'
 
 export default function sendForm(ids = []) {
   return async function action(dispatch) {
-    console.log(228)
     dispatch(setOrderFormStatus('await'))
     setTimeout(
       () => {
